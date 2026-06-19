d05309c17b6f48e7bd44717ffde6a6d9e7f6c5a1
Neffer2 <n3f73r@gmail.com>
2026-06-18 19:36:10 -0500

    1. Actualización copys

--- resources/views/livewire/participants/invoices/upload.blade.php
+++ resources/views/livewire/participants/invoices/upload.blade.php
@@ -64,22 +64,21 @@
     </form>
 </div>
 
-@section('footer')
-    <p class="text-sm text-white text-center">
-        Oferta válida del 15 de Junio de 2026 al 15 de Julio de 2026 <br>
-        <a href="#" class="text-yellow underline">Consulta términos y condiciones</a> en activatupasion.com <br>
+    @section('footer')
+        <p class="text-sm text-white text-center">
 
-        "Lea la etiqueta antes de usar el producto", "Ningún envase o empaque que haya contenido plaguicidas puede
-        usarse para contener alimentos o agua, para consumo humano y animal" y "Manténgase fuera del alcance de los
-        niños, alejado de animales y alimento". "Este(os) producto(s) no puede(n) aplicarse sobre las personas,
-        plantas ni animales, tampoco sobre los alimentos" y "Después de la aplicación debe esperar el tiempo recomendado
-        en la etiqueta antes de ingresar al lugar".
+            "Lea la etiqueta antes de usar el producto", "Ningún envase o empaque que haya contenido plaguicidas puede
+            usarse para contener alimentos o agua, para consumo humano y animal" y "Manténgase fuera del alcance de los
+            niños, alejado de animales y alimento". "Este(os) producto(s) no puede(n) aplicarse sobre las personas,
+            plantas ni animales, tampoco sobre los alimentos" y "Después de la aplicación debe esperar el tiempo recomendado
+            en la etiqueta antes de ingresar al lugar".
+            <br><br>
 
-        <br>
-
-        © 2026 Rapid Repel & Black Flag. Todos los derechos reservados.
-    </p>
-@endsection
+            Oferta válida del 15 de Junio de 2026 al 15 de Julio de 2026.<br>
+            <a href="#" class="text-yellow underline">Consulta términos y condiciones</a> en activatupasion.com <br>
+            © 2026 Rapid Repel & Black Flag. Todos los derechos reservados.
+        </p>
+    @endsection
 
 @script
 <script>
