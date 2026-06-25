971bb769d85d87da2c32f00f7cf90c00652ff633
Neffer2 <n3f73r@gmail.com>
2026-06-25 17:53:34 -0500

    1. Api usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/check-document/{document}', function (string $document) {
+    $exists = User::where('document', $document)->exists();
+
+    return response()->json([
+        'registered' => $exists,
+    ]);
 });
