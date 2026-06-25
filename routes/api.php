7bb153af7ea79c9429433be50f69367d3cee9a14
Neffer2 <n3f73r@gmail.com>
2026-06-25 18:38:12 -0500

    1. Pong post

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/ping', function () {
+Route::post('/ping', function () {
     return response()->json(['message' => 'Hola mundo']);
 });
 
